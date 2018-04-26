0143393e566afa7bc59a607d1c3a74af62891554
batlander <batlander@hotmail.com>
2018-04-26 18:48:58 +0200

    Last

---
+++ Untitled.R
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+test <- testing[,colSums(is.na(testing)) == 0] 
+
+
+iguales <- names(training) = names(examen)
+datos <- training[iguales]
+
+evaluacion <- testing[iguales]
+                      
